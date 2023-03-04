f86905b6a29840ad643205f534e9c251fe470d1b
Linoop Laxman <linoop1989@gmail.com>
2023-03-04 13:02:35 +0530

    New build

--- app/src/main/java/com/example/mycicdapp/presentation/views/MainActivity.kt
+++ app/src/main/java/com/example/mycicdapp/presentation/views/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.mycicdapp.presentation.views
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.mycicdapp.databinding.ActivityMainBinding
 import com.example.mycicdapp.presentation.viewModels.MainViewModel
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -15,6 +19,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        AppCenter.start(
+            application, "7a08bce0-13b6-4ab1-949c-e8de30da2dfa",
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     fun goBack(){
